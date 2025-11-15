bbd8a1bd23f35ac942c20b2d4912e632efc5b66b
Artemio <jjesusartemio16@hotmail.com>
2025-11-15 09:16:09 -0600

    EjemploMatricula

---
+++ src/main/java/come/mx/curso/unidad2/Ejercicio1/main.java
@@ -0,0 +1,11 @@
+package come.mx.curso.unidad2.Ejercicio1;
+
+public class main {
+    static void main(String[] args){
+        Matricula matricula = new Matricula();
+        matricula.setMatricula("UX24NT169");
+
+        System.out.println(matricula.getMatricula());
+
+    }
+}
